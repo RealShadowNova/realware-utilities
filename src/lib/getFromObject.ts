0a23c22dde121d6e160902cfeaf6f67b2ae99ceb
RealShadowNova <real.shadow.nova@gmail.com>
2021-09-02 13:21:40 -0400

    chore: use `hasFromObject()` in `getFromObject()`

--- src/lib/getFromObject.ts
+++ src/lib/getFromObject.ts
@@ -1,3 +1,5 @@
+import { hasFromObject } from './hasFromObject';
+
 /**
  * A semi-complex function to manipulate and get paths in objects.
  * @since 1.0.0
@@ -5,9 +7,11 @@
  * @param path The string path or an array of strings forming a path.
  * @returns The data gotten after manipulation, else undefined.
  */
-
 export function getFromObject<Data extends Record<string, any>>(data: Data | undefined, path: string[] | string): Data | undefined {
 	const fullPath = typeof path === 'string' ? path.replace(/\[/g, '.').replace(/]/g, '').split('.').filter(Boolean) : path;
 
-	return fullPath.every((step) => !(step && (data = data?.[step]) === undefined)) ? data : undefined;
+	return fullPath.reduce<Record<string, any>>(
+		(previousStep, step) => (hasFromObject(previousStep, [step]) ? previousStep[step] : undefined),
+		data ?? {}
+	) as Data;
 }
