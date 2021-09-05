b01451b4911c995914b148bf01f2f60c97fc7ecc
RealShadowNova <real.shadow.nova@gmail.com>
2021-09-05 14:36:13 -0400

    feat: refactor `deleteFromObject()`

--- src/lib/deleteFromObject.ts
+++ src/lib/deleteFromObject.ts
@@ -1,23 +1,26 @@
+import { hasFromObject } from '..';
+
 /**
  * A semi-complex function to manipulate and delete paths in objects.
  * @since 1.0.0
- * @param data The data object to manipulate.
- * @param path The string path or an array of strings forming a path.
- * @returns The manipulated data object.
+ * @param value The value object to manipulate.
+ * @param path An array of strings forming a path.
+ * @returns The manipulated value object.
  */
-export function deleteFromObject<Data extends Record<string, any>>(data: Data, path: string[] | string) {
-	const fullPath = typeof path === 'string' ? path.replace(/\[/g, '.').replace(/]/g, '').split('.').filter(Boolean) : path;
+export function deleteFromObject<InputValue extends Record<string, InputValue | unknown>, OutputValue extends Record<string, OutputValue | unknown>>(
+	value: InputValue,
+	path: string[]
+): OutputValue {
+	if (!path.length) return value as OutputValue;
+	if (!hasFromObject(value, path)) value as OutputValue;
 
-	if (!path.length) return;
+	path.reduce<Record<string, any>>((previousStep, step, index) => {
+		if (typeof previousStep !== 'object') return previousStep;
+		if (Array.isArray(previousStep)) return previousStep;
+		if (index === path.length - 1) Reflect.deleteProperty(previousStep, step);
 
-	fullPath.reduce<Record<string, any>>((previousStep, step, index) => {
-		if (typeof previousStep[step] !== 'object') Reflect.deleteProperty(previousStep, step);
-
-		if (previousStep[step] === undefined) previousStep[step] = {};
-
-		if (index === fullPath.length - 1) Reflect.deleteProperty(previousStep, step);
 		return previousStep[step];
-	}, data);
+	}, value);
 
-	return data;
+	return value as OutputValue;
 }
--- tests/lib/deleteFromObject.test.ts
+++ tests/lib/deleteFromObject.test.ts
@@ -1,8 +1,10 @@
 import { deleteFromObject } from '../../src';
 
-test('GIVEN deleteFromObject() THEN proper data', () => {
-	expect(deleteFromObject({ a: 'b' }, 'a')).toEqual({});
-	expect(deleteFromObject({ a: 'b' }, '[a]')).toEqual({});
-	expect(deleteFromObject({ 0: 'a' }, '[0]')).toEqual({});
-	expect(deleteFromObject({ a: { b: 'c' } }, 'a.b')).toEqual({ a: {} });
+describe('deleteFromObject()', () => {
+	describe('can delete values from objects', () => {
+		test('GIVEN object w/ paths THEN deletes at path', () => {
+			expect(deleteFromObject({ a: 'b' }, ['a'])).toEqual({});
+			expect(deleteFromObject({ a: { b: 'c' } }, ['a', 'b']));
+		});
+	});
 });
