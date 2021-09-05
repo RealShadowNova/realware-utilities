451118d527974bac40266d55502103bac5698b11
RealShadowNova <real.shadow.nova@gmail.com>
2021-09-05 16:08:07 -0400

    feat: remove `setFromObject()`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from './lib/deleteFromObject';
 export * from './lib/getFromObject';
 export * from './lib/hasFromObject';
-export * from './lib/setFromObject';
 export * from './lib/setToObject';
 
 export const version = '[VI]{version}[/VI]';
