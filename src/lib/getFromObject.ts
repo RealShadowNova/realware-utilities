61dda7119a7a3601167bbfd0701d893a78c33e6e
RealShadowNova <real.shadow.nova@gmail.com>
2021-09-05 16:07:00 -0400

    feat: refactor `getFromObject()`

--- src/lib/getFromObject.ts
+++ src/lib/getFromObject.ts
@@ -3,15 +3,16 @@ import { hasFromObject } from './hasFromObject';
 /**
  * A semi-complex function to manipulate and get paths in objects.
  * @since 1.0.0
- * @param data The data object to manipulate.
- * @param path The string path or an array of strings forming a path.
- * @returns The data gotten after manipulation, else undefined.
+ * @param value The value object to manipulate.
+ * @param path An array of strings forming a path.
+ * @returns The value received after manipulation, else undefined.
  */
-export function getFromObject<Data extends Record<string, any>>(data: Data | undefined, path: string[] | string): Data | undefined {
-	const fullPath = typeof path === 'string' ? path.replace(/\[/g, '.').replace(/]/g, '').split('.').filter(Boolean) : path;
-
-	return fullPath.reduce<Record<string, any>>(
+export function getFromObject<
+	InputValue extends Record<string, InputValue | unknown> | (InputValue | unknown)[],
+	OutputValue extends Record<string, OutputValue | unknown> | (OutputValue | unknown)[] = InputValue
+>(value: InputValue, path: string[]): OutputValue | undefined {
+	return path.reduce<Record<string, any>>(
 		(previousStep, step) => (hasFromObject(previousStep, [step]) ? previousStep[step] : undefined),
-		data ?? {}
-	) as Data;
+		value
+	) as OutputValue;
 }
--- tests/lib/getFromObject.test.ts
+++ tests/lib/getFromObject.test.ts
@@ -1,16 +1,54 @@
 import { getFromObject } from '../../src';
 
-test('GIVEN getFromObject() THEN proper data', () => {
-	expect(getFromObject({ a: 'b' }, '')).toEqual({ a: 'b' });
-	expect(getFromObject({ a: 'b' }, 'a')).toBe('b');
-	expect(getFromObject({ a: 'b' }, '[a]')).toBe('b');
-	expect(getFromObject({ a: { b: 'c' } }, 'a.b')).toBe('c');
-	expect(getFromObject({ a: { b: 'c' } }, 'a[b]')).toBe('c');
-	expect(getFromObject({ a: { b: { c: ['d'] } } }, 'a.b.c')).toEqual(['d']);
-	expect(getFromObject({ a: { b: { c: ['d'] } } }, 'a.b.c[0]')).toBe('d');
-	expect(getFromObject({ a: { b: { c: ['d', ['e']] } } }, 'a.b.c[1]')).toEqual(['e']);
-	expect(getFromObject({ a: { b: { c: ['d', ['e']] } } }, 'a.b.c[1][0]')).toBe('e');
-	expect(getFromObject([{ a: 'b' }], '[0].a')).toBe('b');
-	expect(getFromObject(['a', { b: 'c' }], '[1].b')).toBe('c');
-	expect(getFromObject(['a', { b: { c: ['d'] } }], '[1].b.c')).toEqual(['d']);
+describe('GIVEN getFromObject()', () => {
+	describe('can get values from objects', () => {
+		test('GIVEN object w/ empty path THEN gets full value', () => {
+			expect(getFromObject({ a: 'b' }, [])).toEqual({ a: 'b' });
+			expect(getFromObject({ a: { b: 'c' } }, [])).toEqual({ a: { b: 'c' } });
+		});
+
+		test('GIVEN object w/ path THEN gets value at path', () => {
+			expect(getFromObject({ a: 'b' }, ['a'])).toBe('b');
+			expect(getFromObject({ a: { b: 'c' } }, ['a'])).toEqual({ b: 'c' });
+			expect(getFromObject({ a: { b: 'c' } }, ['a', 'b'])).toBe('c');
+		});
+
+		describe('can get values from arrays', () => {
+			test('GIVEN object w/ array and empty path THEN gets full value', () => {
+				expect(getFromObject({ a: ['b'] }, [])).toEqual({ a: ['b'] });
+				expect(getFromObject({ a: { b: ['c'] } }, [])).toEqual({ a: { b: ['c'] } });
+			});
+
+			test('GIVEN object w/ array and path THEN gets value a path', () => {
+				expect(getFromObject({ a: ['b'] }, ['a', '0'])).toBe('b');
+				expect(getFromObject({ a: { b: ['c'] } }, ['a', 'b', '0'])).toBe('c');
+			});
+		});
+	});
+
+	describe('can get values from arrays', () => {
+		test('GIVEN array w/ empty path THEN gets full value', () => {
+			expect(getFromObject(['a'], [])).toEqual(['a']);
+			expect(getFromObject(['a', 'b'], [])).toEqual(['a', 'b']);
+		});
+
+		test('GIVEN array w/ path THEN gets value at path', () => {
+			expect(getFromObject(['a'], ['0'])).toBe('a');
+			expect(getFromObject(['a', 'b'], ['1'])).toBe('b');
+		});
+
+		describe('can get values from objects', () => {
+			test('GIVEN array w/ object and empty path THEN gets full value', () => {
+				expect(getFromObject([{ a: 'b' }], [])).toEqual([{ a: 'b' }]);
+				expect(getFromObject(['a', { b: 'c' }], [])).toEqual(['a', { b: 'c' }]);
+			});
+
+			test('GIVEN array w/ object and path THEN gets value at path', () => {
+				expect(getFromObject([{ a: 'b' }], ['0'])).toEqual({ a: 'b' });
+				expect(getFromObject([{ a: 'b' }], ['0', 'a'])).toBe('b');
+				expect(getFromObject(['a', { b: 'c' }], ['1'])).toEqual({ b: 'c' });
+				expect(getFromObject(['a', { b: 'c' }], ['1', 'b'])).toBe('c');
+			});
+		});
+	});
 });
